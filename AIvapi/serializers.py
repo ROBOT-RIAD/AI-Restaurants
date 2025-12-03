e95502195f3a71b723dedb837461f137554797c4
ROBOT-RIAD <riadhosain420@gmail.com>
2025-12-03 17:50:22 +0600

    update code

--- AIvapi/serializers.py
+++ AIvapi/serializers.py
@@ -8,10 +8,10 @@ class AssistanceCreateSerializer(serializers.Serializer):
     twilio_account_sid = serializers.CharField(max_length=500)
     twilio_auth_token  = serializers.CharField(max_length=500)
 
-    def validate(self, data):
-        if Assistance.objects.filter(twilio_number=data["twilio_number"]).exists():
-            raise serializers.ValidationError({"error": "This Twilio number is already in use."})
-        return data
+    # def validate(self, data):
+    #     if Assistance.objects.filter(twilio_number=data["twilio_number"]).exists():
+    #         raise serializers.ValidationError({"error": "This Twilio number is already in use."})
+    #     return data
 
 
 
