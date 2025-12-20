2744f1bf5ebc7bfeef54e58d2ef4d505c82090e4
ROBOT-RIAD <riadhosain420@gmail.com>
2025-12-20 10:53:26 +0600

    update code

--- AIvapi/views.py
+++ AIvapi/views.py
@@ -101,13 +101,11 @@ class AssistantCreateView(APIView):
         twilio_auth_token = data["twilio_auth_token"]
 
 
-        phone_number_1 = phone_number_1.strip()
-
-        if not re.fullmatch(r'^\+\d{7,18}$', phone_number_1):
+        phone_number_1 = restaurant.phone_number_1
+        print(phone_number_1)
+        if not re.match(r'^\+\d{1,4}\d{6,14}$', phone_number_1):
             return Response(
-                {
-                    "error": f"Invalid phone number format. Example:{phone_number_1}"
-                },
+                {"error": "You must add a country code to the phone number. It should start with a '+' followed by the country code."},
                 status=status.HTTP_400_BAD_REQUEST
             )
 
